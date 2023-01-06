83c6392b1119d98ee363053f8d7d46692d6298d0
Rolo-coding <kay.sothearo2019@rupp.edu.kh>
2023-01-06 14:53:28 +0700

    feat(src): change browser to hash route

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './app'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <HelmetProvider>
         <App />
       </HelmetProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
